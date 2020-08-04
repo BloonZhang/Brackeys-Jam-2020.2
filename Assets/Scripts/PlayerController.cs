d4d06bee0b12057ea129d6cfc5ff398926b4505f
Bloon <BloonZhang@gmail.com>
2020-08-04 02:16:09 -0500

    Movement and level design

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Use CharacterController2D from brackeys video, https://www.youtube.com/watch?v=dwcT-Dch0bA
+    public CharacterController2D controller;
+
+    // public variables
+    public float runSpeed = 30f;
+
+    // helper variables
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get input
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Move. No crouch and no jump
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
