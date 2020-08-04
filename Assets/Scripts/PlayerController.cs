114ca2c9b6ad8b2a5a407bfc70cb86be8c6a54a6
Bloon <BloonZhang@gmail.com>
2020-08-04 02:57:57 -0500

    ghost spawning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,8 +8,12 @@ public class PlayerController : MonoBehaviour
     // Use CharacterController2D from brackeys video, https://www.youtube.com/watch?v=dwcT-Dch0bA
     public CharacterController2D controller;
 
+    // public prefabs
+    public GameObject ghostPrefab;
+
     // public variables
     public float runSpeed = 30f;
+
 
     // helper variables
     float horizontalMove = 0f;
@@ -18,7 +22,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Get input
+        // Get inputs
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
         if (Input.GetButtonDown("Jump"))
@@ -29,8 +33,27 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Move. No crouch and no jump
+        // Move. No crouch
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
         jump = false;
+    }
+
+    // Enter trigger
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        // Spikes
+        if (col.tag == "Spikes")
+        {
+            //Debug.Log("oof");
+            Death();
+        }
+    }
+
+    // Helper methods
+    // Create a ghost and die
+    void Death()
+    {
+        GameObject ghost = (GameObject) Instantiate(ghostPrefab, this.transform.position, Quaternion.identity);
+        ghost.GetComponent<Rigidbody2D>().velocity = this.transform.GetComponent<Rigidbody2D>().velocity;
     }
 }
