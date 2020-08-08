667ba88c9a55ecbf98c349f8e4b543028ae724f1
Bloon <BloonZhang@gmail.com>
2020-08-08 02:03:43 -0500

    Animations and Scenes

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    //////// Singleton shenanigans ////////
+    private static SceneController _instance;
+    public static SceneController Instance { get {return _instance;} }
+    //////// Singleton shenanigans continue in Awake() ////
+
+    void Awake()
+    {
+        // Singleton shenanigans
+        if (_instance != null && _instance != this) {Destroy(this.gameObject);} // no duplicates
+        else {_instance = this;}
+        //DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Hit the start button
+    public void enterIntro()
+    {
+        SceneManager.LoadScene("IntroScreen");
+    }
+    // Finish the intro screen
+    public void enterMainGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    // Replay the game
+    public void restartGame()
+    {
+        enterIntro();
+    }
+    // win the game
+    public void winGame()
+    {
+        SceneManager.LoadScene("WinScene");
+    }
+}
